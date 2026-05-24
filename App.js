cbd1859d77186b417c2bf64de44e123eb2e81796
Priyesh Bhanderi <vyompriyesh@gmail.com>
2026-05-24 18:46:54 +0530

    push

--- App.js
+++ App.js
@@ -1,47 +1,43 @@
 import express from "express";
+import cors from "cors";
 import "dotenv/config";
+import axios from "axios";
+import https from "https";
 import connectDB from "./config/db.js";
 import { sendResponse } from "./utils/response.js";
 import api from "./routes/api.js";
 
 const app = express();
-const port = process.env.PORT || 8000;
-
+const port = process.env.PORT || "8000";
+app.use(
+    cors({
+        origin: [
+            "http://192.168.1.8:5174",
+            "http://localhost:5174",
+            "https://api.rrshoper.in"
+        ],
+        credentials: true,
+        methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+        allowedHeaders: ["Content-Type", "Authorization"],
+    })
+);  
 app.use(express.json());
+connectDB();
 
-app.get("/", (req, res) => {
-    res.send("RR Shoper");
-});
+app.use("/api", api);
 
-// error handler
+
+
+app.get("/", async (req, res) => res.send('RR Shoper'));
+
 app.use((err, req, res, next) => {
-    console.error(err);
-    return sendResponse(
-        res,
-        500,
-        {
-            message: "Something went wrong.",
-            error_message: err.message,
-        },
-        false
-    );
+    console.error(err); // log it
+    return sendResponse(res, 500, {
+        message: 'Something went wrong.',
+        error_message: err.message
+    }, false);
 });
 
-const startServer = async () => {
-    try {
-        await connectDB();
-
-        // ✅ ONLY after DB connected
-        app.use("/api", api);
-
-        app.listen(port, () => {
-            console.log(`server running on ${port}`);
-        });
-
-    } catch (error) {
-        console.log("DB connection failed:", error);
-        process.exit(1);
-    }
-};
-
-startServer();
+app.listen(port, () => {
+    console.log(`server listening at http://localhost:${port}`);
+});
