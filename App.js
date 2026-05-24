f355ad7d3ffde4c26ca8af82f566faa6653673fa
Priyesh Bhanderi <vyompriyesh@gmail.com>
2026-05-24 18:55:43 +0530

    check

--- App.js
+++ App.js
@@ -9,7 +9,30 @@ import api from "./routes/api.js";
 
 const app = express();
 const port = process.env.PORT || "8000";
-app.use(cors());
+const allowedOrigins = [
+    "http://192.168.1.8:5174",
+    "http://localhost:5174",
+    "https://rrshoper.in",
+];
+
+const corsOptions = {
+    origin: function (origin, callback) {
+        // allow Postman, mobile apps, curl
+        if (!origin) return callback(null, true);
+
+        if (allowedOrigins.includes(origin)) {
+            return callback(null, true);
+        }
+
+        // IMPORTANT: do NOT throw error, just block silently
+        return callback(null, false);
+    },
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"]
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 connectDB();
 
